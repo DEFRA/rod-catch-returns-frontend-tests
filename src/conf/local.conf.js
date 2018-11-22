dc0da185960b544b9fffa8ace1be3eae3978475a
Sam Gardner-Dell <sgardnerdell.ea@gmail.com>
2018-11-22 18:54:36 +0000

    Run firefox with headless flags

--- src/conf/local.conf.js
+++ src/conf/local.conf.js
@@ -21,7 +21,7 @@ const localConfig = {
       browserName: 'firefox',
       'moz:firefoxOptions': {
         // flag to activate Firefox headless mode (see https://github.com/mozilla/geckodriver/blob/master/README.md#firefox-capabilities for more details about moz:firefoxOptions)
-        // args: ['--headless']
+        args: ['--headless']
       }
     }
   ],
