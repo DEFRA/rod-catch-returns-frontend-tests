c16bcfebf6151177eadd4f76c6be1fce947312b2
Sam Gardner-Dell <sgardnerdell.ea@gmail.com>
2019-03-15 14:36:23 +0000

    Improve valid date calculation for large catch page

--- src/js/step_definitions/7-Large-fish/I_select_a_valid_catch_date.js
+++ src/js/step_definitions/7-Large-fish/I_select_a_valid_catch_date.js
@@ -2,23 +2,26 @@
 const { defineStep } = require('cucumber')
 const moment = require('moment')
 const LargeCatch = require('../../pages/Large-Catches.page')
-defineStep('I select a valid catch date', function () {
-  // Get the current season from the page
-  let submissionYear = LargeCatch.getYear()
-  let today = moment()
 
-  if (submissionYear === today.year()) {
-    // Need to pick a date between 1 Jan and today
-    let startOfYear = moment().startOf('year')
-    let daysDiff = today.diff(startOfYear, 'days')
-    let randomNumberOfDaysAgo = Math.floor(Math.random() * daysDiff)
-    let catchDate = today.subtract(randomNumberOfDaysAgo, 'days')
-    LargeCatch.setDate(catchDate.date(), catchDate.month())
-  } else {
-    // Any date of the year will be valid (submission for last season)
-    let startOfYear = moment([submissionYear, 1, 1])
-    let randomDays = Math.floor(Math.random() * 365)
-    let catchDate = startOfYear.add(randomDays, 'days')
-    LargeCatch.setDate(catchDate.date(), catchDate.month())
+function getLatestValidDateForSubmissionYear (submissionYear) {
+  // default to today
+  let latestDate = moment()
+  // if the submission is for a previous year, the latest valid date will be the end of that year
+  if (submissionYear < latestDate.year()) {
+    latestDate = moment([submissionYear]).endOf('year')
   }
+  return latestDate
+}
+
+function getValidDate (submissionYear) {
+  let startOfYear = moment([submissionYear])
+  let latestValid = getLatestValidDateForSubmissionYear(submissionYear)
+  let daysDiff = latestValid.diff(startOfYear, 'days')
+  let randomDays = Math.floor(Math.random() * daysDiff)
+  return startOfYear.add(randomDays, 'days')
+}
+
+defineStep('I select a valid catch date', function () {
+  let catchDate = getValidDate(parseInt(LargeCatch.getYear()))
+  LargeCatch.setDate(catchDate.format('D'), catchDate.format('M'))
 })
