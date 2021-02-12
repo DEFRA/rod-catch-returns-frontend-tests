d5254d336a2fbcd7a3492c83ae14fc0d9e736f36
nabeelamir-defra <nabeel.amir@defra.gov.uk>
2021-02-12 09:29:56 +0000

    fixed linting issues

---
+++ src/js/step_definitions/11-Records/I_should_see_the_license_number_records_page.js
@@ -0,0 +1,7 @@
+
+'use strict'
+const { defineStep } = require('cucumber')
+
+defineStep('I should see the license number records page', function () {
+  expect(browser).toHaveTitleContaining('Enter a licence number - GOV.UK')
+})
