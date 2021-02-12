85285d53e93496a1365e2f4b1e036c7861ba6fba
nabeelamir-defra <nabeel.amir@defra.gov.uk>
2021-02-12 10:34:43 +0000

    fixed linting

--- src/js/step_definitions/11-Records/I_should_not_see_the_license_number_records_page.js
+++ src/js/step_definitions/11-Records/I_should_not_see_the_license_number_records_page.js
@@ -1,6 +1,5 @@
 'use strict'
 const { defineStep } = require('cucumber')
-const expectChai = require('chai').expect
 
 defineStep('I should not see the license number records page', function () {
   expect(browser).not.toHaveTitleContaining('Enter a licence number - GOV.UK')
--- src/js/step_definitions/11-Records/I_should_see_the_license_number_records_page.js
+++ src/js/step_definitions/11-Records/I_should_see_the_license_number_records_page.js
@@ -1,6 +1,5 @@
 'use strict'
 const { defineStep } = require('cucumber')
-const expectChai = require('chai').expect
 
 defineStep('I should see the license number records page', function () {
   expect(browser).toHaveTitleContaining('Enter a licence number - GOV.UK')
