9205f621200903e96e7a723356713ec479be45e3
naimullakhandefra <77017471+naimullakhandefra@users.noreply.github.com>
2021-02-12 10:30:35 +0000

    Renamed the folders

--- src/js/step_definitions/11-Records/I_should_not_see_the_license_number_records_page.js
+++ src/js/step_definitions/11-Records/I_should_not_see_the_license_number_records_page.js
@@ -1,5 +1,6 @@
 'use strict'
 const { defineStep } = require('cucumber')
+const expectChai = require('chai').expect
 
 defineStep('I should not see the license number records page', function () {
   expect(browser).not.toHaveTitleContaining('Enter a licence number - GOV.UK')
