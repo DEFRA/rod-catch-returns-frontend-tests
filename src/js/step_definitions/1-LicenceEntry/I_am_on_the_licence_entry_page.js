1b25a99d8902d0c17c93c1b254f0c35dcd731484
Phil Benson <phil.benson@defra.gov.uk>
2024-05-24 15:57:24 +0100

    Added log of licence entry html

--- src/js/step_definitions/1-LicenceEntry/I_am_on_the_licence_entry_page.js
+++ src/js/step_definitions/1-LicenceEntry/I_am_on_the_licence_entry_page.js
@@ -3,5 +3,7 @@ const { defineStep } = require("@cucumber/cucumber")
 const LicencePage = require('../../pages/Licence-page')
 
 defineStep('I am on the licence entry page', async function () {
+  const base = await $('html')
+  console.log('html:', base)
   await LicencePage.checkOpen()
 })
