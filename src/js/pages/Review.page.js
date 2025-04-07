ab221d650f32b56511f41197ae497eea92babf04
Nabeel Amir <73947018+nabeelamir-defra@users.noreply.github.com>
2025-04-07 14:09:24 +0100

    Locked submission (#50)
    * Locked submission
    
    https://eaflood.atlassian.net/browse/IWTF-4613
    
    Add acceptance test for locked submission
    
    * added test for locked submission
    
    * ignoring order of table

--- src/js/pages/Review.page.js
+++ src/js/pages/Review.page.js
@@ -10,6 +10,19 @@ class ReviewPage extends Page {
     const clickCancel = browser.element('#return-summary')
     await clickCancel.click()
   }
+
+  async validateTableByCaption (captionText, dataTable) {
+    // Find the table by its caption
+    const table = await $(`caption*=${captionText}`).parentElement()
+
+    const expectedRows = dataTable.raw().slice(1) // Skip the first row (headers), we just want to validate the content
+    const tbodyText = await table.$('tbody').getText() // convert table to text
+
+    for (const row of expectedRows) {
+      const rowText = row.filter(cell => cell !== '<any>').join(' ')
+      expect(tbodyText).toContain(rowText) // assert table row contains text in table, because the order of the table can be random
+    }
+  }
 }
 
 module.exports = new ReviewPage()
