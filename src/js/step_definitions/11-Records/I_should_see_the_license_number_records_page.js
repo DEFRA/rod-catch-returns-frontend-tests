af68fcd05f16715babed18e157545baacdb3fd55
Iris Faraway <iris.faraway@gmail.com>
2026-09-10 16:44:42 +0100

    Update service name in tests (#93)
    https://eaflood.atlassian.net/browse/IWTF-5291
    
    We recently changed the name of the RCR service, which broke some tests that were looking for it in the title attribute.

--- src/js/step_definitions/11-Records/I_should_see_the_license_number_records_page.js
+++ src/js/step_definitions/11-Records/I_should_see_the_license_number_records_page.js
@@ -1,5 +1,5 @@
 import { defineStep } from '@cucumber/cucumber'
 
 defineStep('I should see the license number records page', async function () {
-  await expect(browser).toHaveTitle('Enter a licence number - Report your salmon or sea trout catch - GOV.UK')
+  await expect(browser).toHaveTitle('Enter a licence number - Report your salmon and sea trout fishing activity - GOV.UK')
 })
